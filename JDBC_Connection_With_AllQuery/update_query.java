e78dad23eddc03f1ef5bdb5fd851bef53fd40c7d
Gunjan Katre <gunjankatre55@gmail.com>
2025-06-02 15:19:53 +0530

    Add files via upload

---
+++ JDBC_Connection_With_AllQuery/update_query.java
@@ -0,0 +1,44 @@
+package com.tka;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
+public class update_query 
+{
+	public static void main(String[] args) throws Exception
+	{
+		
+Class.forName("com.mysql.cj.jdbc.Driver");
+		
+		String url="jdbc:mysql://localhost:3306/infosys?useSSL=false";
+		String user="root";
+		String Pass="Gunjankatre";
+		
+		
+		Connection c=DriverManager.getConnection(url, user, Pass);
+	
+         if(c!=null)
+         {
+        	System.out.println("Database connected!");
+        	
+        	String query="update employee set  name='Akhi' where id=1;";
+        	PreparedStatement stmt=c.prepareStatement(query);
+        	
+        	 int num=stmt.executeUpdate();
+        	 
+        	 System.out.println(num);
+        	 System.out.println("Employee modified!");
+        	
+        	
+         }
+         else
+        	 System.out.println("Databse NotConnected!");
+         
+         
+	}
+
+}
+
+
+
